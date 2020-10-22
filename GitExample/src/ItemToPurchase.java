f93ba9ec674f88c7eeb4a372782935a9414c11c3
chchchchchchc <73006776+chchchchchchc@users.noreply.github.com>
2020-10-22 11:24:07 -0400

    put all variable in correct place

--- GitExample/src/ItemToPurchase.java
+++ GitExample/src/ItemToPurchase.java
@@ -1,12 +1,12 @@
 
 public class ItemToPurchase {
+	String variable;
+	int abd;
+	int c;
+	int abc;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String variable;
-		int abd;
-		int c;
-		int abc;
 	}
 
 }
